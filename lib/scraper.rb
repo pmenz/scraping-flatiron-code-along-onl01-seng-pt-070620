5d041cc27de7e1bce7168006aa24010e1fb5e911
Pablo Menz <p.menz.r@gmail.com>
2020-08-15 04:53:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -28,7 +28,7 @@ class Scraper
   end
   
   def print_courses
-    self.make_courses
+     self.make_courses
     Course.all.each do |course|
       if course.title && course.title != ""
         puts "Title: #{course.title}"
